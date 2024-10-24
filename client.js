56f29f67ec93eebaa6ab0bbd891d01e1d450c650
Vitor <vi7org@hotmail.com>
2024-10-24 12:30:06 -0400

    basic file server implementation

---
+++ client.js
@@ -0,0 +1,21 @@
+﻿const net = require('net');
+
+const PORT = 8080;
+const HOST = 'localhost';
+
+const client = new net.Socket();
+
+const filepath = "./test.txt";
+
+client.setEncoding('utf8');
+
+client.connect(PORT, HOST, ()=>{
+  console.log(`Connected to server at ${HOST}:${PORT}`);
+
+  client.write(filepath);
+});
+
+client.on('data',(data)=>{
+  console.log(data);
+});
+
---
+++ server.js
@@ -0,0 +1,28 @@
+﻿const net = require('net');
+const PORT = 8080;
+const HOST = 'localhost';
+
+const fs = require('fs');
+
+const server = net.createServer((socket)=>{
+  console.log("Client connected");
+
+  socket.setEncoding('utf8');
+
+
+  socket.on('data', (data)=>{
+    fs.readFile(data, 'utf8', (err, d)=>{
+      if (err) {
+        console.log(`Error reading file: ${err}`);
+        socket.write(`Error reading file: ${err}`);
+      }
+      socket.write(d);
+      console.log(`File sent to client successfully`);
+    });
+
+  });
+});
+
+server.listen(PORT, HOST, ()=>{
+  console.log(`Server listening on ${HOST}:${PORT}`);
+});
